b1bad88397473fbab0c6fa472dd399a6a999813e
Lê Bá Văn <vancrus12@gmail.com>
2025-02-13 00:47:36 +0700

    Ví dụ Hello
    Demo

---
+++ HelloWorld/src/vanleba/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package vanleba.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
